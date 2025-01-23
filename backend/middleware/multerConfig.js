815c73450c7d35e62d035a13901d95ba19a86d8e
diwanshu <diwanshu63019@gmail.com>
2025-01-24 00:12:50 +0530

    add: temp/ file generatpr in multer

--- backend/middleware/multerConfig.js
+++ backend/middleware/multerConfig.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
 const multer = require('multer');
+
+// Ensure the directory exists
+const createTempDir = () => {
+  const dir = 'temp/';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
 
 // Configure multer to store files temporarily
 const storage = multer.diskStorage({
-  
   destination: (req, file, cb) => {
+    createTempDir(); // Create directory if it doesn't exist
     console.log('inside multer middleware')
     cb(null, 'temp/'); // Temporary storage folder
   },
