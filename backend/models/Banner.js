36113c5cb2b7a2220402d9cbd837b53b9a39c2d2
diwanshu <diwanshu63019@gmail.com>
2025-02-10 13:44:21 +0530

    fix: saving default status of banner

--- backend/models/Banner.js
+++ backend/models/Banner.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bannerSchema = new mongoose.Schema({
   title: { type: String, required: true },
   isDefault: { type: Boolean, default: false },
-  status: { type: String, enum: ['Active', 'Upcoming', 'Inactive']},
+  status: { type: String, enum: ['Active', 'Upcoming', 'Inactive'], default: 'Inactive'},
   photoWeb: { type: String },
   photoApp: { type: String },
   pages: {type: String},
